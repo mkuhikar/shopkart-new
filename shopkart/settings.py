14a1f44286654e5ecfd99e851de0238c636c3647
Piyush Kuhikar <mkuhikar@gmail.com>
2022-01-26 17:38:17 +0530

    allowed host

--- shopkart/settings.py
+++ shopkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ['django-env.eba-jkpzaxna.us-west-2.elasticbeanstalk.com','*']
+ALLOWED_HOSTS = ['shopkart-env.eba-4kp65mgx.us-west-2.elasticbeanstalk.com','*']
 
 # Application definition
 
